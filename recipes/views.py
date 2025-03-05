f05f1b4cfe9d854ffc4fa9a65fed65dc91165301
Behlica <niveasouza86@gmail.com>
2025-03-05 15:04:10 -0300

    adicionando pacotes html, css e fontes

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,4 @@ from django.shortcuts import HttpResponse
 
 def home (request): 
     return render(request, 'home.html')
-def sobre (request): 
-    return HttpResponse('sobre')
-def contato (request):
-    return HttpResponse('contato')
+
